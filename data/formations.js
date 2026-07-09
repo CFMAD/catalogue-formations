d235738049476600faf1c51523f4b3af6fddec3f
CFMAD <formation@serfam.be>
2026-07-09 23:06:29 +0200

    Update formations.js

--- data/formations.js
+++ data/formations.js
@@ -1,1 +1,48 @@
+const formations = [
 
+{
+    id: 1,
+    categorie: "Prévention, santé et sensibilisation",
+    titre: "Prévention des chutes",
+    duree: "3 heures",
+    public: "Professionnels de terrain",
+    description: "Identifier les facteurs de risque et mettre en œuvre des actions de prévention."
+},
+
+{
+    id: 2,
+    categorie: "Cadre professionnel",
+    titre: "Secret professionnel",
+    duree: "6 heures",
+    public: "Professionnels de terrain",
+    description: "Cadre légal, secret partagé et responsabilité professionnelle."
+},
+
+{
+    id: 3,
+    categorie: "Travailler en équipe",
+    titre: "Communication en équipe",
+    duree: "1 journée",
+    public: "Professionnels de terrain",
+    description: "Développer une communication efficace au sein des équipes."
+},
+
+{
+    id: 4,
+    categorie: "Le métier à domicile et ses spécificités pratiques",
+    titre: "L'accompagnement au domicile",
+    duree: "1 journée",
+    public: "Aides familiales",
+    description: "Bonnes pratiques de l'accompagnement au domicile."
+},
+
+{
+    id: 5,
+    categorie: "Les packs professionnels",
+    titre: "Pack Alzheimer",
+    duree: "3 journées",
+    public: "Professionnels de terrain",
+    description: "Cycle complet consacré à l'accompagnement des personnes atteintes de troubles cognitifs."
+}
+
+];

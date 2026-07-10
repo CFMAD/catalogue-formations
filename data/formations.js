e9f575f7738bddd62c4c5d95e12db2955808769e
CFMAD <formation@serfam.be>
2026-07-10 10:02:06 +0200

    Update formations.js

--- data/formations.js
+++ data/formations.js
@@ -4,15 +4,19 @@ const formations = [
     id: 1,
     categorie: "Prévention, santé et sensibilisation",
     titre: "Prévention des chutes",
+    image: "images/batiment.jpg",
     duree: "3 heures",
     public: "Professionnels de terrain",
-    description: "Identifier les facteurs de risque et mettre en œuvre des actions de prévention."
+    description: "Identifier les facteurs de risque et mettre en œuvre des actions de prévention.",
+    lien: "formation.html?id=1"
 },
 
 {
     id: 2,
+    lien: "formation.html?id=2",
     categorie: "Cadre professionnel",
     titre: "Secret professionnel",
+    image: "images/formation.jpg",
     duree: "6 heures",
     public: "Professionnels de terrain",
     description: "Cadre légal, secret partagé et responsabilité professionnelle."
@@ -20,8 +24,10 @@ const formations = [
 
 {
     id: 3,
+    lien: "formation.html?id=3",
     categorie: "Travailler en équipe",
     titre: "Communication en équipe",
+    image: "images/document.jpg",
     duree: "1 journée",
     public: "Professionnels de terrain",
     description: "Développer une communication efficace au sein des équipes."
@@ -29,8 +35,10 @@ const formations = [
 
 {
     id: 4,
+    lien: "formation.html?id=4",
     categorie: "Le métier à domicile et ses spécificités pratiques",
     titre: "L'accompagnement au domicile",
+    image: "images/ecrire.jpg",
     duree: "1 journée",
     public: "Aides familiales",
     description: "Bonnes pratiques de l'accompagnement au domicile."
@@ -38,8 +46,10 @@ const formations = [
 
 {
     id: 5,
+    lien: "formation.html?id=5",
     categorie: "Les packs professionnels",
     titre: "Pack Alzheimer",
+    image: "images/groupe.jpg",
     duree: "3 journées",
     public: "Professionnels de terrain",
     description: "Cycle complet consacré à l'accompagnement des personnes atteintes de troubles cognitifs."

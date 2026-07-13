cb9b67d305d5e1ce363812c02fe8d669daa9950e
CFMAD <formation@serfam.be>
2026-07-13 11:41:46 +0200

    Update formation.js

--- js/formation.js
+++ js/formation.js
@@ -12,31 +12,26 @@ if (!formation) {
 
 }
 
-const params = new URLSearchParams(window.location.search);
-
-const id = Number(params.get("id"));
-
-const formation = formations.find(f => f.id === id);
-
 document.getElementById("titre").textContent = formation.titre;
 
 document.getElementById("categorie").textContent = formation.categorie;
-  document.getElementById("imageFormation").src = formation.image;
-  document.getElementById("imageFormation").alt = formation.titre;
+
+document.getElementById("imageFormation").src = formation.image;
+document.getElementById("imageFormation").alt = formation.titre;
 
 document.getElementById("presentation").textContent = formation.presentation;
 
 document.getElementById("objectifs").innerHTML =
-formation.objectifs.replace(/\n/g,"<br>");
+    formation.objectifs.replace(/\n/g,"<br>");
 
 document.getElementById("formateur").textContent =
-formation.formateur;
+    formation.formateur;
 
 document.getElementById("public").textContent =
-formation.public;
+    formation.public;
 
 document.getElementById("participants").textContent =
-formation.participants;
+    formation.participants;
 
 document.getElementById("tarif").textContent =
-formation.tarif;
+    formation.tarif;

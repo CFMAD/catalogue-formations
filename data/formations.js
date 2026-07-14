dfae27667705ba815c1366e8ea383081773b5920
CFMAD <formation@serfam.be>
2026-07-14 12:15:05 +0200

    Update formations.js

--- data/formations.js
+++ data/formations.js
@@ -38,53 +38,353 @@ participants: "max 18 personnes",
     date: "Modulables, en fonction de vos disponibilités et de celles du formateur" 
 },
 
-
 {
-    id: 3,
-    lien: "formation.html?id=3",
-    categorie: "Travailler en équipe",
-    titre: "Communication en équipe",
-    image: "images/document.jpg",
-    duree: "1 journée",
-    public: "Professionnels de terrain",
-    description: "Développer une communication efficace au sein des équipes.",
-    objectifs: `
-• Comprendre les facteurs de risque
-• Identifier les situations dangereuses
-• Adapter ses pratiques
+id: 3,
+categorie: "Prévention, santé et sensibilisation ",
+titre: "Agressivité du bénéficiaire",
+image: "images/formation.jpg",
+presentation: " comprendre et anticiper les situations d’agressivité, de mieux gérer leurs émotions et d’acquérir des techniques concrètes pour prévenir et gérer efficacement les situations de crise avec les bénéficiaires
+.",
+objectifs: `
+•	Acquérir des outils permettant de gérer ses émotions ;
+•	Identifier et anticiper les causes de l’agressivité du bénéficiaire;
+•	Gérer la crise sur base de différentes techniques ;
+•	Définition et causes de l’agressivité du bénéficiaire et de soi ;
+•	Les différentes techniques de gestion de crise.
 `,
+formateur: "Madame Peeters ou Madame Bordenga",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: "Modulables, en fonction de vos disponibilités et de celles du formateur"
 },
 
 {
-    id: 4,
-    lien: "formation.html?id=4",
-    categorie: "Le métier à domicile et ses spécificités pratiques",
-    titre: "L'accompagnement au domicile",
-    image: "images/ecrire.jpg",
-    duree: "1 journée",
-    public: "Aides familiales",
-    description: "Bonnes pratiques de l'accompagnement au domicile.",
-    objectifs: `
-• Comprendre les facteurs de risque
-• Identifier les situations dangereuses
-• Adapter ses pratiques
-`,
+id: 4,
+categorie: "Prévention, santé et sensibilisation ",
+titre: "Sexualité des bénéficiaires",
+image: "images/formation.jpg",
+presentation: "Aborder des situations sensibles dans l’accompagnement des bénéficiaires et des familles. Identifier les situations à risque et d’adapter les pratiques et les réponses dans le respect et l’éthique
+ .",
+objectifs: `
+•	Famille et problèmes liés à la sexualité
+•	Identifier les déviances
+•	La place de la sexualité
+•	Identifier et comprendre les leviers afin de positionner sa pratique dans un cadre professionnel adéquat`,
+formateur: "Madame Cotton ou Monsieur Zumbo",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: "Modulables, en fonction de vos disponibilités et de celles du formateur"
 },
 
 {
-    id: 5,
-    lien: "formation.html?id=5",
-    categorie: "Les packs professionnels",
-    titre: "Pack Alzheimer",
-    image: "images/groupe.jpg",
-    duree: "3 journées",
-    public: "Professionnels de terrain",
-    description: "Cycle complet consacré à l'accompagnement des personnes atteintes de troubles cognitifs.",
-    objectifs: `
-• Comprendre les facteurs de risque
-• Identifier les situations dangereuses
-• Adapter ses pratiques
+id: 5,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " La place de la spiritualité chez le bénéficiaire ",
+image: "images/formation.jpg",
+presentation: "Mieux repérer et comprendre les questionnements existentiels et spirituels des bénéficiaires et de leur entourage, grâce à des outils d’identification et d’exploration favorisant un accompagnement adapté et bienveillant.",
+objectifs: `
+•	Former les personnes à l'usage et à la compréhension de deux groupes d'outils; 
+•	Permettre de s'ouvrir à un regard et un langage différent; 
+•	détecter, repérer, identifier les questions existentielles ou spirituelles des bénéficiaires et leur entourage 
 `,
-}
+formateur: "Madame Coumans",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: "Lundi entre 11h et 17h / Mardi PM / Jeudi PM"
+},
 
+{
+id: 6,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " Se régénérer par la neuroplasticité. ",
+image: "images/formation.jpg",
+presentation: " Stimuler la plasticité neuronale afin de favoriser le bien-être. Les bienfaits de la respiration en toute conscience. Exercices de détente et de relaxation. Mise en mouvements spécifiques. Les duos en miroir ",
+objectifs: `
+•	Mettre le corps en mouvements corporels avec des exercices qui sortent des habitudes et qui améliorent la plasticité neuronale;
+•	Permettre aux neurones de se régénérer
+•	Permettre de s'ouvrir à un regard et un langage différent; 
+`,
+formateur: "Madame Coumans",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: "Lundi entre 11h et 17h / Mardi PM / Jeudi PM"
+},
+
+{
+id: 7,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " Le deuil ",
+image: "images/formation.jpg",
+presentation: " Cette formation propose un espace de compréhension et de réflexion pour mieux appréhender les réalités de la perte, tant pour soi que dans l'accompagnement des autres.",
+objectifs: `
+•	Les étapes et le mécanisme du deuil. Comment gérer
+•	Comment annoncer une perte?
+•	Comment fonctionnent les émotions?
+`,
+formateur: "Monsieur Zumbo",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur. Attention, il faut prévoir minimum 3h45 pour cette formation "
+},
+
+{
+id: 8,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " Premiers secours ",
+image: "images/formation.jpg",
+presentation: " Réagir efficacement face à une situation critique et adopter les bons réflexes afin de protéger les personnes, limiter les conséquences d'un accident et favoriser une prise en charge rapide.",
+objectifs: `
+•	Identifier une situation d'urgence
+•	Assurer sa propre sécurité et celle de la victime
+•	Alerter les services de secours
+•	Effectuer les gestes de premiers secours
+•	Prévenir les accidents domestiques
+`,
+formateur: "Madame Saeys",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur. "
+},
+
+{
+id: 9,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " Maladie de Parkinson ",
+image: "images/formation.jpg",
+presentation: " Mieux comprendre les réalités vécues par les bénéficiaires afin de favoriser un accompagnement adapté, bienveillant et respectueux de leurs besoins.",
+objectifs: `
+•	Acquérir des connaissances sur la maladie et ses symptômes
+•	Comprendre ses conséquences et adapter son accompagnement pour garantir la qualité de vie du bénéficiaire
+`,
+formateur: "Madame Emmanuele",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur – Lundi ou mardi. "
+},
+
+{
+id: 10,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " Maladies mentales ",
+image: "images/formation.jpg",
+presentation: " Meilleure compréhension des principaux troubles psychiques afin favoriser une attitude adaptée, sécurisante et bienveillante dans l'accompagnement des bénéficiaires.",
+objectifs: `
+•	Identifier, différencier et comprendre les maladies mentales
+•	Adapter son intervention à la maladie mentale et gérer la crise
+•	Distinguer le "normal" et la pathologique
+(ex: dépression, bipolarité, troubles anxieux, psychose, schizophrénie, alcoolisme,...)
+`,
+formateur: "Madame Godart",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur. "
+},
+
+{
+id: 11,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " Maladies cardio-vasculaires ",
+image: "images/formation.jpg",
+presentation: " mieux comprendre leurs enjeux afin de favoriser un accompagnement préventif, sécurisant et adapté aux besoins des bénéficiaires au quotidien.",
+objectifs: `
+•	Connaitre et reconnaitre les risques cardiovasculaires, leurs facteurs et importance de la prévention.
+•	Soutenir l'hygiène de vie du bénéficiaire et favoriser la qualité de vie
+•	Accompagner dans l'autogestion de la maladie. Savoir réagir en cas d'urgence
+`,
+formateur: "Madame Godart",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur. "
+},
+
+{
+id: 12,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " Troubles cognitifs ",
+image: "images/formation.jpg",
+presentation: " Cette formation offre des repères pour mieux comprendre les réalités des troubles cognitifs et favoriser un accompagnement respectueux, sécurisant et centré sur les besoins de la personne et de son entourage
+.",
+objectifs: `
+•	Comprendre les troubles (causes, les types...)
+•	Adapter l'accompagnement du bénéficiaire et de son environnement
+•	Gestion des situations et des crises et accompagnement bientraitant
+•	Soutenir les proches
+`,
+formateur: "Madame Wellens ou Madame Bordenga",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur. "
+},
+
+{
+id: 13,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " Hygiène et prévention dans le métier ",
+image: "images/formation.jpg",
+presentation: " Cette formation sensibilise aux bonnes pratiques professionnelles permettant de préserver la santé, la sécurité et le bien-être tant des bénéficiaires que des intervenants
+.",
+objectifs: `
+•	Identifier les règles d'hygiène indispensables dans la pratique quotidienne;
+•	Comprendre les principaux risques professionnels et les moyens de prévention;
+•	Appliquer des gestes simples et efficaces pour garantir la sécurité de tous
+`,
+formateur: "Madame Saeys ",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur. "
+},
+
+{
+id: 14,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " Accidents domestiques ",
+image: "images/formation.jpg",
+presentation: " Cette formation permet de renforcer la vigilance au quotidien et de contribuer à un environnement de vie plus sûr pour les bénéficiaires comme pour les intervenants
+.",
+objectifs: `
+•	Identifier les principaux risques d'accidents domestiques au domicile;
+•	Adopter des comportements préventifs pour réduire les risques;
+•	Réagir de manière appropriée en cas d'accident
+`,
+formateur: "Madame Saeys ou Madame Godart",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur. "
+},
+
+{
+id: 15,
+categorie: "Prévention, santé et sensibilisation ",
+titre: " Le diabète ",
+image: "images/formation.jpg",
+presentation: " Cette formation permet de mieux comprendre les réalités vécues par les personnes diabétiques afin de leur offrir un accompagnement adapté, attentif et bienveillant
+
+.",
+objectifs: `
+•	Comprendre les bases du diabète et ses impacts sur la vie quotidienne ;
+•	Identifier les signes d’alerte et les situations à risque ;
+•	Adapter son accompagnement aux besoins des personnes diabétiques ;
+•	Rôle de l’aide familiale dans l’accompagnement et la vigilance au quotidien ;
+`,
+formateur: "Madame Saeys ",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur. "
+},
+
+{
+id: 16,
+categorie: "Cadre professionnel ",
+titre: " Affirmation de soi : comment poser mes limites ?",
+image: "images/formation.jpg",
+presentation: " Cette formation invite à développer une communication équilibrée et respectueuse de soi comme des autres, afin de renforcer sa confiance, préserver ses ressources personnelles et construire des relations professionnelles plus sereines
+.",
+objectifs: `
+•	Comprendre la différence entre assertivité, inhibition et agressivité
+•	Acquérir des outils pour apprendre à poser ses limites de manière claire et adaptée
+•	Améliorer son assertivité, communiquer un refus de manière claire et respectueuse
+
+`,
+formateur: "Madame Peeters ",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur. "
+},
+
+{
+id: 17,
+categorie: "Cadre professionnel ",
+titre: " La charge mentale - Comment regagner l'équilibre ?",
+image: "images/formation.jpg",
+presentation: " Cette formation invite à prendre du recul sur les sollicitations du quotidien et leurs impacts sur le bien-être, afin de retrouver un fonctionnement plus serein, renforcer ses ressources personnelles et préserver un équilibre durable entre les différentes sphères de vie
+.",
+objectifs: `
+•	Identifier les différents signes de surcharges mentales
+•	Réfléchir aux différentes pistes de solutions
+•	Apprendre à ralentir le flux des idées
+•	Se détendre par la respiration
+`,
+formateur: "Madame Coumans ",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Lundi entre 11h et 17h / Mardi PM / Jeudi PM "
+},
+
+{
+id: 18,
+categorie: "Cadre professionnel ",
+titre: " Les couleurs de la vie comme outil de bien-être",
+image: "images/formation.jpg",
+presentation: " cette formation explore comment les couleurs peuvent devenir de véritables alliées pour favoriser l'harmonie, l'expression de soi et le bien-être au quotidien
+.",
+objectifs: `
+•	Identifier les couleurs comme outils de bien-être
+•	S'aider soi-même par les couleurs
+•	Savoir distinguer les 2 formes de rapports aux couleurs
+•	Identifier l'utilisation quotidienne des couleurs
+`,
+formateur: "Madame Coumans ",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Lundi entre 11h et 17h / Mardi PM / Jeudi PM "
+},
+
+{
+id: 19,
+categorie: "Cadre professionnel ",
+titre: " Déontologie, communication et devoir de discrétion ",
+image: "images/formation.jpg",
+presentation: " Cette formation permet de mieux appréhender le cadre professionnel, les responsabilités liées à la fonction et les principes éthiques indispensables à une intervention respectueuse et de qualité
+.",
+objectifs: `
+•	Repérer et contourner les pièges déontologiques les plus fréquents
+•	Comprendre et mesurer la responsabilité dans les actes
+•	Définition du cadre déontologique, du secret professionnel, du devoir de discrétion et des implications
+`,
+formateur: "Madame Bordenga ou Madame Coumans ",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Modulables, en fonction de vos disponibilités et de celles du formateur. "
+},
+
+{
+id: 20,
+categorie: "Cadre professionnel ",
+titre: " Booster la confiance en soi ",
+image: "images/formation.jpg",
+presentation: " Cette formation offre un espace de réflexion et d'expérimentation pour renforcer son épanouissement personnel et professionnel
+.",
+objectifs: `
+•	Distinguer confiance en soi et estime de soi
+•	Identifier les croyances limitantes et acceptation de soi
+•	Outils et techniques, plan d'actions et mises en pratique
+`,
+formateur: "Madame Coumans ",
+public: "Professionnels de terrain, AF, AMS, AS",
+participants: "max 18 personnes",
+tarif: "Pour une offre sur mesure, le tarif vous sera communiqué sur simple demande selon vos besoins",
+date: " Lundi entre 11h et 17h / Mardi PM / Jeudi PM "
+},
+
+
+
+
+    
 ];
